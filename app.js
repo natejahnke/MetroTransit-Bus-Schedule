fb3c064174a5bbf70c25e7b3beb2250df2a11bc0
Nathan Jahnke <naterased@gmail.com>
2018-07-09 23:07:20 -0500

    more progress

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 var inquirer = require("inquirer");
 var axios = require("axios");
 
-var preguntas = [
-  {
-    type: "input",
-    name: "route",
-    message: "Enter bus route number."
-  },
-  {
-    when: function(response) {
-      axios
-        .get(
-          "http://svc.metrotransit.org/NexTrip/Directions/" +
-            response.route +
-            "?format=json"
-        )
-        .then(function(direction) {
-          const directionOne = direction.data[0].Text;
-          const directionTwo = direction.data[1].Text;
-          console.log(directionOne + " " + directionTwo);
-        });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "route",
+      message: "Enter bus route number."
+    },
+    {
+      when: function(response) {
+        axios
+          .get(
+            "http://svc.metrotransit.org/NexTrip/Directions/" +
+              response.route +
+              "?format=json"
+          )
+          .then(function(direction) {
+            const directionOne = direction.data[0].Text;
+            const directionTwo = direction.data[1].Text;
+          });
+      }
+    },
+    {
+      type: "list",
+      name: "direction",
+      message: "Choose a direction.",
+      choices: ["1", "South", new inquirer.Separator(), "East", "West"]
+    },
+    {
+      type: "input",
+      message: "stop",
+      name: "stop",
+      message: "Enter your stop ID on route."
     }
-  },
-  {
-    type: "list",
-    name: "direction",
-    message: "Choose a direction.",
-    choices: ["North", "South", new inquirer.Separator(), "East", "West"]
-  },
-  {
-    type: "input",
-    message: "stop",
-    name: "stop",
-    message: "Enter your stop ID on route."
-  }
-];
-inquirer.prompt(preguntas, function(respuestas) {
-  console.log(respuestas);
-});
+  ])
+  .then(function(bus) {
+    console.log(bus.route);
+    axios.get(
+      "http://svc.metrotransit.org/NexTrip/" +
+        bus.route +
+        "/" +
+        bus.direction +
+        "/" +
+        bus.stop +
+        "?format=json"
+    );
+  });
